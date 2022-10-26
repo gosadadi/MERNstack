9e5c7883f68338c9f9ac000d68b90bd844c8559f
gosa_dadi <gosa.d.dadi@gmail.com>
2022-10-26 11:13:20 -0700

    MERN

---
+++ createjsreact/index.js
@@ -0,0 +1,24 @@
+
+class ExampleButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { isCLicked: false };
+    }
+    render() {
+        if (this.state.isCLicked) {
+            return 'You clicked this';
+        }
+        return React.createElement(
+            'button',
+            { onClick: () => this.setState({ isCLicked: true }) },
+            'Click me!'
+        );
+    }
+}
+const domContainer = document.querySelector('#root');
+// create the root of react dom
+const root = ReactDOM.createRoot(domContainer);
+// render what we see
+root.render(React.createElement(ExampleButton));    
+
+
