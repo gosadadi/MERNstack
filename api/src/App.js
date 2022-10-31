cba6acb95242d6fa124c8888cb06a8c9e42bf9f5
gosa_dadi <gosa.d.dadi@gmail.com>
2022-10-31 15:47:22 -0700

    Pokemon API

---
+++ api/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PokemonAPI from './components/PokemonAPI'
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      < PokemonAPI/>
+    </div>
+  );
+}
+
+export default App;
