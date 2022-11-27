f4d8624ecdea9be730c964183ede8598cd72b9cb
gosa_dadi <gosa.d.dadi@gmail.com>
2022-11-27 11:01:14 -0800

    updated

--- inventory-app/client/src/components/product/productForm/ProductForm.jsx
+++ inventory-app/client/src/components/product/productForm/ProductForm.jsx
@@ -1,6 +1,6 @@
 
-import React, { useState } from 'react';
-import ReactQuill from 'react-quill';
+import React from "react";
+import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import Card from "../../card/Card";
 import "./ProductForm.scss";
@@ -13,40 +13,76 @@ const ProductForm = ({
     setDescription,
     handleInputChange,
     handleImageChange,
-    saveProduct
+    saveProduct,
 }) => {
     return (
-        <div className='add-product'>
+        <div className="add-product">
             <Card cardClass={"card"}>
                 <form onSubmit={saveProduct}>
                     <Card cardClass={"group"}>
                         <label>Product Image</label>
-                        <code className='--color-dark'>Supported Formats: jpg, jpeg, png</code>
-                        <input type="file" name="image" onChange={(e) => handleImageChange(e)} />
+                        <code className="--color-dark">
+                            Supported Formats: jpg, jpeg, png
+                        </code>
+                        <input
+                            type="file"
+                            name="image"
+                            onChange={(e) => handleImageChange(e)}
+                        />
+
                         {imagePreview != null ? (
                             <div className="image-preview">
-                                <img src={imagePreview} alt="product image" />
+                                <img src={imagePreview} alt="product" />
                             </div>
-                        ) : (<p>No image set for this product</p>)}
+                        ) : (
+                            <p>No image set for this poduct.</p>
+                        )}
                     </Card>
-                    <label>Product Name</label>
-                    <input type="text" placeholder='Product name' name="name" value={product?.name}
-                        onChange={handleInputChange} />
-                    <label>Product Category :</label>
-                    <input type="text" placeholder='Product category' name="category" value={product?.category}
-                        onChange={handleInputChange} />
-                    <label>Product Price</label>
-                    <input type="text" placeholder='price' name="price" value={product?.price}
-                        onChange={handleInputChange} />
-                    <label>Product quantity</label>
-                    <input type="text" placeholder='Product quantity' name="quantity" value={product?.quantity}
-                        onChange={handleInputChange} />
-                    <label>Product Description</label>
-                    <ReactQuill theme="snow"
-                        value={description} onChange={setDescription}
-                        modules={ProductForm.modules} 
+                    <label>Product Name:</label>
+                    <input
+                        type="text"
+                        placeholder="Product name"
+                        name="name"
+                        value={product?.name}
+                        onChange={handleInputChange}
+                    />
+
+                    <label>Product Category:</label>
+                    <input
+                        type="text"
+                        placeholder="Product Category"
+                        name="category"
+                        value={product?.category}
+                        onChange={handleInputChange}
+                    />
+
+                    <label>Product Price:</label>
+                    <input
+                        type="text"
+                        placeholder="Product Price"
+                        name="price"
+                        value={product?.price}
+                        onChange={handleInputChange}
+                    />
+
+                    <label>Product Quantity:</label>
+                    <input
+                        type="text"
+                        placeholder="Product Quantity"
+                        name="quantity"
+                        value={product?.quantity}
+                        onChange={handleInputChange}
+                    />
+
+                    <label>Product Description:</label>
+                    <ReactQuill
+                        theme="snow"
+                        value={description}
+                        onChange={setDescription}
+                        modules={ProductForm.modules}
                         formats={ProductForm.formats}
                     />
+
                     <div className="--my">
                         <button type="submit" className="--btn --btn-primary">
                             Save Product
@@ -55,8 +91,9 @@ const ProductForm = ({
                 </form>
             </Card>
         </div>
-    )
+    );
 };
+
 ProductForm.modules = {
     toolbar: [
         [{ header: "1" }, { header: "2" }, { font: [] }],
@@ -94,4 +131,4 @@ ProductForm.formats = [
     "align",
 ];
 
-export default ProductForm
+export default ProductForm;
